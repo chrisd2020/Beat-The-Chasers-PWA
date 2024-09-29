4a1b36ed4517bf9616847083a19378eb7fedb79d
Chris <chrisd2020@gmail.com>
2024-09-30 00:09:10 +0100

    Initial (Logo and buttons which print clicker)

---
+++ Beat The Chasers/src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+  function onClick (clicker) {
+    console.log(clicker);
+  };
+</script>
+
+<template>
+  <div class="container">
+    <img class="logo" src="./assets/beat_the_chasers.jpg" />
+    <button @click="onClick('player')">
+      Player
+    </button>
+    <button @click="onClick('chaser')">
+      Chaser
+    </button>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+button {
+  width: 200px;
+  height: 50px;
+  display: block;
+}
+
+.container {
+  height: auto;
+  width: 100%;
+}
+
+h1 {
+  margin: 0;
+}
+
+.logo {
+  width: 100%;
+  height: auto;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
